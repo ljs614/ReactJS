53dfc7ed2914d7f6efc956875f2de10ada9e316f
jeongseop.lee <jeongseop.lee@wizontech.com>
2020-03-28 19:33:20 +0900

    React Router

--- movie_app_2020/src/App.js
+++ movie_app_2020/src/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
-
+import { HashRouter, Route } from "react-router-dom";
+import About from "./routes/About";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail"; 
+import Navigation from "./components/Navigation";
 
 function App() {
-  return <span>something</span>
+  return (
+    <HashRouter>
+      <Navigation />
+      <Route path="/" component={Home} exact={true} />
+      <Route path="/about" component={About} />
+      <Route path="/movie/:id" component={Detail} />
+    </HashRouter>
+  )
 }
 
 export default App;
